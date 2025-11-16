459105e74cca2c346d92eea9a370034ae4b97633
britto007 <shafkathossainbritto007@gmail.com>
2025-11-17 00:19:53 +0600

    home screen control

---
+++ GPA_Calculator_Builderss/GPA_Calculator_Builder/src/main/java/gpaapp/HomeController.java
@@ -0,0 +1,17 @@
+package gpaapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeController {
+    @FXML
+    void startApp(ActionEvent e) throws Exception {
+        Stage stage = (Stage)((javafx.scene.Node)e.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gpaapp/course_entry.fxml"));
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("GPA Calculator - Courses");
+    }
+}
